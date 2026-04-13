af91cfcdcf048ecebbf8df54e7dbac23dda396fa
JavierNLopez <l23210612@tectijuana.edu.mx>
2026-04-13 21:25:29 +0000

    Cambios en settings

--- mymovies/settings.py
+++ mymovies/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "98.95.252.95"]
 print(ALLOWED_HOSTS)
 
 
